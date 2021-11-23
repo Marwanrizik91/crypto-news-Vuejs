0c1081fe2feedc2b08aaae1fc387aaccbd27ab84
Marwanrizik91 <marwan.rizik@gmail.com>
2021-11-23 08:11:01 +0200

    more generic component changes

--- src/components/TheSkeleton.vue
+++ src/components/TheSkeleton.vue
@@ -1,43 +1,31 @@
 <template>
-    <div class="theSkeleton_component" :style="{width: width + 'px', height: height + 'px', borderRadius: br +'px', margin: margin + 'px', background, border}">
-        <div class="skeleton_content"></div>
-    </div>
+  <div
+    class="theSkeleton_component"
+    :style="styles"
+  >
+    <div class="skeleton_content"></div>
+  </div>
 </template>
 
 <script>
 export default {
   name: "TheSkeleton",
   props: {
-    width: {
-        type: Number,
-        default: 100
+    styles: {
+        type: Object,
+        default: () => {
+            return {
+                width: '100px',
+                height: '100px',
+                borderRadius: '15px',
+                margin: '10px',
+                background: '#ff5c5c',
+                border: '2px solid #C4C4C4'
+            }
+        }
     },
-    height: {
-        type: Number,
-        default: 100
-    },
-    br: {
-        type: Number,
-        default: 15
-    },
-    margin: {
-        type: Number,
-        default: 10
-    },
-    background: {
-        type: String,
-    },
-    border: {
-        type: String,
-        default: "solid 2px #C4C4C4"
-    }
-
   },
 };
 </script>
 
-<style scoped>
-    .theSkeleton_component {
-        background: var(--skinRed);
-    }
-</style>
+<style scoped></style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,10 +2,12 @@
   <div class="home">
     <div class="information_bar"></div>
     <div class="display_home_grid">
-
-    <Skeleton class="grid_element1" :width="style.mainBlock.width" :height="style.mainBlock.height">
-        
-    </Skeleton>
+      <Skeleton
+        class="card_container"
+        :style="styles"
+      >
+        <Skeleton></Skeleton>
+      </Skeleton>
     </div>
   </div>
 </template>
@@ -14,38 +16,38 @@
 // @ is an alias to /src
 
 export default {
-  name: 'Home',
+  name: "Home",
 
   data() {
     return {
-      style: {
-        mainBlock: {
-          width: 'auto',
-          height: 450
-        }
-      }
-    }
+      styles: {
+        
+          width: "auto",
+          height: '450px',
+        
+      },
+    };
   },
 
   components: {
-    Skeleton: () => import('../components/TheSkeleton.vue')
+    Skeleton: () => import("../components/TheSkeleton.vue"),
   },
   computed: {
-    selectedItemComputed: function() {
-      return this.$store.getters.getSelectedItem.toLowerCase()
-    }
+    selectedItemComputed: function () {
+      return this.$store.getters.getSelectedItem.toLowerCase();
+    },
   },
-}
+};
 </script>
 
 <style scoped>
 .display_home_grid {
   display: grid;
-  grid-template-columns: 1fr 1.5fr 1fr;
-  grid-template-rows:1fr 1fr ;
+  grid-template-columns: 1fr 2fr 1fr;
+  grid-template-rows: 1fr 1fr;
 }
 
-.grid_element1 {
+.card_container {
   grid-column-start: 2;
 }
 </style>
